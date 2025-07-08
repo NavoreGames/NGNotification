98f6d99005e9574b3b7024307a4a8b4ee9c76e4c
NavoreGames <navoregames@gmail.com>
2025-07-08 19:24:49 -0300

    adicionado propriedade Sujestion no NGException

--- NGNotification/Domain/Models/NGException.cs
+++ NGNotification/Domain/Models/NGException.cs
@@ -5,15 +5,18 @@ namespace NGNotification.Models
     public class NGException : NGMessage
     {
         public string Trace { get; set; }
+        public string Sujestion { get; set; }
 
-        public NGException(string header, string message, string trace)
+        public NGException(string header, string message, string sujestion, string trace)
         {
             Category = Category.Error;
             Header = header;
             Message = message;
+            Sujestion = sujestion;
             Trace = trace;
         }
-        public NGException(string header, string message) : this(header, message, "") { }
+        public NGException(string header, string message, string sujestion) : this(header, message, sujestion, "") { }
+        public NGException(string header, string message) : this(header, message, "","") { }
         public NGException(string message) : this("", message) { }
         public NGException() { }
 
